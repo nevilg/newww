34de0f7851fc9a314b56f94c3c43f63128e46ec6
Nevil George <liven93@gmail.com>
2016-11-24 18:12:00 -0800

    changing intro

--- app/components/Intro.jsx
+++ app/components/Intro.jsx
@@ -8,7 +8,7 @@ class Intro extends React.Component {
       <div className="col-md-12 intro-text">
         <h1 className="animated fadeInDown">nevil george</h1>
         <h3>computer science at northwestern university</h3>
-        <h3>software engineer at uber</h3>
+        <h3>software engineering at uber</h3>
 
         <div className="icons">
             <a href="http://linkedin.com/in/nevilgeorge" target="_blank"><i className="fa fa-linkedin"></i></a>
