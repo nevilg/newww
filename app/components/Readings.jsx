1b12062d64596faedaec6946aba597bba6142cf3
Nevil George <liven93@gmail.com>
2017-12-11 21:38:15 -0800

    add readings.jsx

---
+++ app/components/Readings.jsx
@@ -0,0 +1,16 @@
+// Readings.jsx
+
+import React from 'react';
+import ContentPage from './ContentPage.jsx';
+
+class Readings extends React.Component {
+  render() {
+    return (
+      <ContentPage
+        title='readings'
+      />
+    );
+  }
+}
+
+export default Readings;
